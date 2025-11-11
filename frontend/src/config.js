a5cab020ed531c1c3f3df56ecf0efa17e65696a8
dyyeon <dohy3141@naver.com>
2025-11-11 21:56:52 +0900

    new branch (db connection)

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,6 +1,6 @@
 // src/config.js
 const API_BASE =
   import.meta.env.VITE_API_BASE_URL || // ✅ Vite 환경변수 (없으면 아래 fallback)
-  'http://localhost:5000';
+  "https://commensurately-preflagellate-merissa.ngrok-free.dev/";
 
 export { API_BASE };
